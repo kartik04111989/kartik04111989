bf4d597b9beb87e00f9554898da6ed8de68f3651
kartik04111989 <111672564+kartik04111989@users.noreply.github.com>
2024-02-06 08:21:57 +0530

    Add files via upload

---
+++ removeDuplicate.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(numbers) {
+    if (numbers.length === 0) return 0;
+    
+    let i = 0;
+    
+    for (let j = 1; j < numbers.length; j++) {
+        if (numbers[j] !== numbers[i]) {
+            i++;
+            numbers[i] = numbers[j];
+        }
+    }
+    
+    return i + 1;
+};
